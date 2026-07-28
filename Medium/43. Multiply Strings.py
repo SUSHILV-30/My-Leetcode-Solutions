ccf2342955807d6e0963bfd8825e55e9099001d0
SUSHIL V <sushilv2005@gmail.com>
2026-07-28 16:54:07 +0530

    Implement string multiplication in Multiply Strings

---
+++ Medium/43. Multiply Strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        num11 = int(num1)
+        num21 = int(num2)
+        
+        res = num11 * num21
+
+        res1 = str(res)
+
+        return res1
