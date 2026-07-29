f7c73162f7205dd7388123b744fd1e87c7754a3b
SUSHIL V <sushilv2005@gmail.com>
2026-07-29 15:42:14 +0530

    Implement binary search in a 2D matrix

---
+++ Medium/74. Search a 2D Matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        low,high = 0,m*n-1
+
+        while low<=high:
+            mid = (low+high)//2
+
+            row = mid//n
+            col = mid%n
+            val = matrix[row][col]
+
+            if val == target:
+                return True
+            elif val < target:
+                low = mid+1
+
+            else:
+                high = mid-1
+        return False
+
+        
